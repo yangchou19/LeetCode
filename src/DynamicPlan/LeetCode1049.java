e04abe3992f8dfd4e3e312c7b591f7ef3d0f8270
Yang <1178574011@qq.com>
2023-02-22 12:06:51 +0800

    day43

---
+++ src/DynamicPlan/LeetCode1049.java
@@ -0,0 +1,30 @@
+package DynamicPlan;
+
+public class LeetCode1049 {
+    /**
+     * 有一堆石头，用整数数组 stones 表示。其中 stones[i] 表示第 i 块石头的重量。
+     *
+     * 每一回合，从中选出任意两块石头，然后将它们一起粉碎。
+     * 假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+     *
+     * 如果 x == y，那么两块石头都会被完全粉碎；
+     * 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+     * 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
+     *
+     */
+    public int lastStoneWeightII(int[] stones) {
+        // 尽量让石头分成重量相同的两堆，相撞之后剩下的石头最小
+        int sum =0;
+        for(int stone:stones){
+            sum+=stone;
+        }
+        int target = sum >> 1;
+        int[] dp = new int[target+1];
+        for(int i=0; i<stones.length; i++){
+            for(int j=target; j>=stones[i];j--){
+                dp[j] = Math.max(dp[j], dp[j-stones[i]]+stones[i]);
+            }
+        }
+        return sum - 2*dp[target];
+    }
+}
